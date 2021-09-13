3616051da5c99de084ce4acce186cc9af3d47e0f
Micheal kuo <sxdcfv20002000@gmail.com>
2021-09-13 14:35:54 +0800

    “0913PM02“

--- routes/address-book.js
+++ routes/address-book.js
@@ -12,11 +12,21 @@ router.get('/list', async (req, res)=>{
     res.locals.pageName = 'ab-list';
     const perPage = 5;
     let page = parseInt(req.query.page) || 1;
+    let keyword = req.query.keyword || '';
+
+    res.locals.keyword = keyword; // 傳給 template
+
     const output = {
 
     };
 
-    const t_sql = "SELECT COUNT(1) totalRows FROM address_book ";
+    let where = " WHERE 1 ";
+    if(keyword){
+        where += ` AND \`name\` LIKE ${ db.escape('%' + keyword + '%') } `;
+    }
+
+
+    const t_sql = `SELECT COUNT(1) totalRows FROM address_book ${where}`;
     const [[{totalRows}]] = await db.query(t_sql);
     output.totalRows = totalRows;
     output.totalPages = Math.ceil(totalRows/perPage);
@@ -33,7 +43,7 @@ router.get('/list', async (req, res)=>{
             return res.redirect('?page=' + output.totalPages);
         }
 
-        const sql = `SELECT * FROM \`address_book\` ORDER BY sid DESC LIMIT ${(page-1)*perPage}, ${perPage}`;
+        const sql = `SELECT * FROM \`address_book\` ${where} ORDER BY sid DESC LIMIT ${(page-1)*perPage}, ${perPage}`;
         const [rows] = await db.query(sql)
         output.rows = rows;
 
@@ -63,10 +73,9 @@ router.route('/add')
             success: false,
         }
 
-        //方法1
         // const sql = "INSERT INTO `address_book`(" +
         //     "`name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES (?, ?, ?, ?, ?, NOW())";
-
+        //
         // const [result] = await db.query(sql, [
         //     req.body.name,
         //     req.body.email,
@@ -75,12 +84,15 @@ router.route('/add')
         //     req.body.address,
         // ]);
 
-        //...req.body 先展開 ＝>name`, `email`, `mobile`, `birthday`, `address`  + created_at: new Date()
-        
         const input = {...req.body, created_at: new Date()};
         const sql = "INSERT INTO `address_book` SET ?";
-        const [result] = await db.query(sql,[input]);
-
+        let result = {};
+        // 處理新增資料時可能的錯誤
+        try {
+            [result] = await db.query(sql, [input]);
+        } catch(ex){
+            output.error = ex.toString();
+        }
         output.result = result;
         if(result.affectedRows && result.insertId){
             output.success = true;
@@ -103,6 +115,45 @@ router.route('/add')
         res.json(output);
     });
 
+
+router.route('/edit/:sid')
+    .get(async (req, res)=>{
+        const sql = "SELECT * FROM address_book WHERE sid=?";
+        const [rs] = await db.query(sql, [req.params.sid]);
+
+        if(rs.length){
+            res.render('address-book/edit', {row: rs[0]});
+        } else {
+            res.redirect('/address-book/list');
+        }
+    })
+    .post(async (req, res)=>{
+        // TODO: 欄位檢查
+        const output = {
+            success: false,
+            postData: req.body,
+        }
+
+        const input = {...req.body};
+        const sql = "UPDATE `address_book` SET ? WHERE sid=?";
+        let result = {};
+        // 處理修改資料時可能的錯誤
+        try {
+            [result] = await db.query(sql, [input, req.params.sid]);
+        } catch(ex){
+            output.error = ex.toString();
+        }
+        output.result = result;
+        if(result.affectedRows===1){
+            if(result.changedRows===1){
+                output.success = true;
+            } else {
+                output.error = '資料沒有變更';
+            }
+        }
+
+        res.json(output);
+    });
 
 
 module.exports = router;
