a93267906602d3d4f9e923508df39fb71b0e0505
Micheal kuo <sxdcfv20002000@gmail.com>
2021-09-13 11:47:34 +0800

    “0913AM11“

--- routes/address-book.js
+++ routes/address-book.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./../modules/connect-mysql');
+const upload = require('./../modules/upload-images');
 
 const router = express.Router();
 
@@ -32,7 +33,7 @@ router.get('/list', async (req, res)=>{
             return res.redirect('?page=' + output.totalPages);
         }
 
-        const sql = `SELECT * FROM \`address_book\` LIMIT ${(page-1)*perPage}, ${perPage}`;
+        const sql = `SELECT * FROM \`address_book\` ORDER BY sid DESC LIMIT ${(page-1)*perPage}, ${perPage}`;
         const [rows] = await db.query(sql)
         output.rows = rows;
 
@@ -41,6 +42,67 @@ router.get('/list', async (req, res)=>{
     // res.json(output);
     res.render('address-book/list', output);
 });
+
+
+router.delete('/delete/:sid([0-9]+)', async (req, res)=>{
+    const sql = "DELETE FROM address_book WHERE sid=?";
+
+    const [r] = await db.query(sql, [req.params.sid]);
+    console.log({r});
+    res.json(r);
+});
+
+router.route('/add')
+    .get(async (req, res)=>{
+        res.locals.pageName = 'ab-add';
+        res.render('address-book/add');
+    })
+    .post(async (req, res)=>{
+        // TODO: 欄位檢查
+        const output = {
+            success: false,
+        }
+
+        //方法1
+        // const sql = "INSERT INTO `address_book`(" +
+        //     "`name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES (?, ?, ?, ?, ?, NOW())";
+
+        // const [result] = await db.query(sql, [
+        //     req.body.name,
+        //     req.body.email,
+        //     req.body.mobile,
+        //     req.body.birthday,
+        //     req.body.address,
+        // ]);
+
+        //...req.body 先展開 ＝>name`, `email`, `mobile`, `birthday`, `address`  + created_at: new Date()
+        
+        const input = {...req.body, created_at: new Date()};
+        const sql = "INSERT INTO `address_book` SET ?";
+        const [result] = await db.query(sql,[input]);
+
+        output.result = result;
+        if(result.affectedRows && result.insertId){
+            output.success = true;
+        }
+
+        console.log({result});
+        /*
+        {
+          result: ResultSetHeader {
+            fieldCount: 0,
+            affectedRows: 1,
+            insertId: 148,
+            info: '',
+            serverStatus: 2,
+            warningStatus: 0
+          }
+        }
+         */
+
+        res.json(output);
+    });
+
 
 
 module.exports = router;
